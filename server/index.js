44162c9c5b61ea67abef5e085cf6c7373e797576
Syed Noor ul Absar <syedabsar99@gmail.com>
2026-05-24 14:52:00 +0530

    Add programmatic DNS resolution override for MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
--- server/test_db.js
+++ server/test_db.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 const mongoose = require('mongoose');
 
 async function test() {
